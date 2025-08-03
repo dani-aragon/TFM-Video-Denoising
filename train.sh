ebf8d7d4a0606004b1d75a2c55c68d1dd33c9c4d
dani-aragon <daragonsantin@gmail.com>
2025-08-03 20:40:55 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 ##############################################################################
-# This script receives one argument:
+# This script receives two arguments:
 # 1. The model to be trained (autoencoder or diffusion).
+# 2. The GPU(s) to use.
 ##############################################################################
 
 echo "Training ${1}."
@@ -10,11 +11,13 @@ echo "Training ${1}."
 # conda init
 # conda activate video
 
-# export ROOT_TFM="/LUSTRE/users/daragon/TFM-Video-Diffusion/data"
-# export PYTHONPATH=$PYTHONPATH:/LUSTRE/users/daragon/TFM-Video-Diffusion
-export ROOT_TFM="/mnt/d/Documentos/TFM/TFM-Video-Diffusion/data"
-export PYTHONPATH=$PYTHONPATH:/mnt/d/Documentos/TFM/TFM-Video-Diffusion
+GPU=${2:-0}
+export CUDA_VISIBLE_DEVICES=$GPU
+export ROOT_TFM="/LUSTRE/users/daragon/TFM-Video-Diffusion/data"
+export PYTHONPATH=$PYTHONPATH:/LUSTRE/users/daragon/TFM-Video-Diffusion
+# export ROOT_TFM="/mnt/d/Documentos/TFM/TFM-Video-Diffusion/data"
+# export PYTHONPATH=$PYTHONPATH:/mnt/d/Documentos/TFM/TFM-Video-Diffusion
 
 # Execute python script.
-# python /LUSTRE/users/daragon/TFM-Video-Diffusion/${1}/train.py
-python /mnt/d/Documentos/TFM/TFM-Video-Diffusion/${1}/train.py
+python /LUSTRE/users/daragon/TFM-Video-Diffusion/${1}/train.py
+# python /mnt/d/Documentos/TFM/TFM-Video-Diffusion/${1}/train.py
